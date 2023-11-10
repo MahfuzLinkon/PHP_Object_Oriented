261ea29b4f28ab09fe5658c3cfea6c63e5ef4ea0
mahfuzlinkon <mahfuzur076@gmail.com>
2023-11-10 22:11:13 +0600

    Php namespace

---
+++ namespace/src/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../PaymentGateway/Stripe/Transaction.php';
+require_once '../PaymentGateway/Paddle/Transaction.php';
+require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+
+var_dump(new PaymentGateway\Paddle\Transaction());
+
